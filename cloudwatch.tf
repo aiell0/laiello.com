d9f3c1f9fb8938df64dceab9ff4c9056fb9b39d0
aiell0 <lawrence.aiello@laiello.com>
2020-04-26 17:07:52 -0400

    Add all metrics

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -1,10 +1,10 @@
 resource "aws_cloudwatch_metric_alarm" "cpu_anomaly_detection" {
   alarm_name                = "laiello-com-cpu-detection"
   comparison_operator       = "GreaterThanUpperThreshold"
-  evaluation_periods        = "5"
+  evaluation_periods        = "3"
   threshold_metric_id       = "e1"
   alarm_description         = "This metric monitors ec2 cpu utilization"
-  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
 
   metric_query {
     id          = "e1"
@@ -33,10 +33,10 @@ resource "aws_cloudwatch_metric_alarm" "cpu_anomaly_detection" {
 resource "aws_cloudwatch_metric_alarm" "network_in_anomaly_detection" {
   alarm_name                = "laiello-com-network-detection"
   comparison_operator       = "GreaterThanUpperThreshold"
-  evaluation_periods        = "5"
+  evaluation_periods        = "3"
   threshold_metric_id       = "e1"
   alarm_description         = "This metric monitors inbound network traffic"
-  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
 
   metric_query {
     id          = "e1"
@@ -68,35 +68,44 @@ resource "aws_cloudwatch_metric_alarm" "cpu_credit_alarm" {
   evaluation_periods        = "2"
   metric_name               = "CPUCreditBalance"
   namespace                 = "AWS/EC2"
-  period                    = "300"
+  period                    = "120"
   statistic                 = "Average"
   threshold                 = "50"
   alarm_description         = "This metric monitors ec2 credit balance"
-  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
+  dimensions = {
+    InstanceId = aws_instance.ec2.id
+  }
 }
 
 resource "aws_cloudwatch_metric_alarm" "instance_status_check_alarm" {
   alarm_name                = "instance_status_check_alarm"
-  comparison_operator       = "GreaterThanThreshold"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "StatusCheckFailed_Instance"
   namespace                 = "AWS/EC2"
-  period                    = "60"
+  period                    = "120"
   statistic                 = "Sum"
   threshold                 = "1"
   alarm_description         = "This metric monitors instance status check failures"
-  insufficient_data_actions = []
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
+  dimensions = {
+    InstanceId = aws_instance.ec2.id
+  }
 }
 
 resource "aws_cloudwatch_metric_alarm" "system_status_check_alarm" {
   alarm_name                = "system_status_check_alarm"
-  comparison_operator       = "GreaterThanThreshold"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = "1"
   metric_name               = "StatusCheckFailed_System"
   namespace                 = "AWS/EC2"
-  period                    = "60"
+  period                    = "120"
   statistic                 = "Sum"
   threshold                 = "1"
-  alarm_description         = "This metric monitors instance status check failures"
-  insufficient_data_actions = []
+  alarm_description         = "This metric monitors system status check failures"
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
+  dimensions = {
+    InstanceId = aws_instance.ec2.id
+  }
 }
