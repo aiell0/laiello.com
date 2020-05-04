08f3b40569e4b80f3b55161277410514ba5e281c
aiell0 <lawrence.aiello@laiello.com>
2020-05-04 08:37:38 -0400

    add disk and memory usage alarms

--- cloudwatch.tf
+++ cloudwatch.tf
@@ -36,7 +36,7 @@ resource "aws_cloudwatch_metric_alarm" "cpu_credit_alarm" {
   evaluation_periods        = "3"
   metric_name               = "CPUCreditBalance"
   namespace                 = "AWS/EC2"
-  period                    = "120"
+  period                    = "60"
   statistic                 = "Average"
   threshold                 = "50"
   alarm_description         = "This metric monitors ec2 credit balance"
@@ -52,7 +52,7 @@ resource "aws_cloudwatch_metric_alarm" "instance_status_check_alarm" {
   evaluation_periods        = "1"
   metric_name               = "StatusCheckFailed_Instance"
   namespace                 = "AWS/EC2"
-  period                    = "120"
+  period                    = "60"
   statistic                 = "Sum"
   threshold                 = "1"
   alarm_description         = "This metric monitors instance status check failures"
@@ -68,10 +68,42 @@ resource "aws_cloudwatch_metric_alarm" "system_status_check_alarm" {
   evaluation_periods        = "1"
   metric_name               = "StatusCheckFailed_System"
   namespace                 = "AWS/EC2"
-  period                    = "120"
+  period                    = "60"
   statistic                 = "Sum"
   threshold                 = "1"
   alarm_description         = "This metric monitors system status check failures"
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
+  dimensions = {
+    InstanceId = aws_instance.ec2.id
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "disk_usage_alarm" {
+  alarm_name                = "disk_usage_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "disk_used_percent"
+  namespace                 = "CWAgent"
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = "60"
+  alarm_description         = "This metric monitors disk space usage"
+  alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
+  dimensions = {
+    InstanceId = aws_instance.ec2.id
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "memory_usage_alarm" {
+  alarm_name                = "memory_usage_alarm"
+  comparison_operator       = "GreaterThanOrEqualToThreshold"
+  evaluation_periods        = "3"
+  metric_name               = "mem_used_percent"
+  namespace                 = "CWAgent"
+  period                    = "60"
+  statistic                 = "Average"
+  threshold                 = "70"
+  alarm_description         = "This metric monitors RAM usage"
   alarm_actions             = [aws_sns_topic.laiello_infrastructure.arn]
   dimensions = {
     InstanceId = aws_instance.ec2.id
