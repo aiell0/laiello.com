e41079a704b7470d6a0939365a55ff7b352961fa
aiell0 <lawrence.aiello@laiello.com>
2023-03-11 22:20:58 -0500

    chore: add default variables; update provider version

--- variables.tf
+++ variables.tf
@@ -1,9 +1,11 @@
 variable "region" {
   type        = string
   description = "AWS region"
+  default     = "us-east-1"
 }
 
 variable "instance_size" {
   type        = string
   description = "Size of AWS EC2 instance."
+  default     = "t3a.small"
 }
